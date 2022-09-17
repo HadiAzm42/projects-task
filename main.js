730f550331fe82644c0f41ec3fe02cc2a5cab059
Hadi Azeem <hadiazeem.azeemi@gmail.com>
2022-09-18 00:22:42 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+const form = document.getElementById("Form");
+const input = document.getElementById("Input");
+const taskList = document.getElementById("Task_List");
+
+form.onsubmit = () => {
+    e.preventDefault();
+    addTask();
+};
+
+function addTask() {
+    let taskValue = input.value;
+    // if(task) {
+    //     taskValue = task.text;
+    // }
+    task ? (taskValue = task.text) : task;
+    console.log(taskValue);
+    taskValue ? displayTask() : taskValue;
+
+    function displayTask() {
+        const newTask = document.createElement("li");
+
+        newTask.innerHTML = taskValue;
+
+
+        newTask.onclick = () => {
+            newTask.classList.toggle("completed");
+            newTask.classList.contains('completed') ? ((newTask.innerHTML = `<span>${taskValue} &#10004;</span><span onclick='deleteTask()'>&#10060</span>`), newTask.classList.toggle("delete_task")):(newTask.innerHTML = `${taskValue}`, newTask.classList.toggle("delete_task"));
+        };
+        taskList.appendChild(newTask);
+
+        input.value = "";
+    }
+}
+
+// delete task function
+
+function deleteTask() {
+    console.log(document.querySelectorAll("li"));
+
+    document.querySelectorAll('li').forEach((element) => {
+        element.classList.contains('delete_task') ? element.remove() : element;
+    })
+}
